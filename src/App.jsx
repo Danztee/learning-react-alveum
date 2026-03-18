e985617a8d9c454a46a724f939d2b8c770eb89b5
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-03-18 14:49:58 +0100

    handling data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import StepOne from "./components/steps/stepOne";
 import StepThree from "./components/steps/StepThree";
 import StepTwo from "./components/steps/StepTwo";
+import Todo from "./components/Todo";
 
 function App() {
   const [step, setStep] = useState(1);
@@ -27,16 +28,19 @@ function App() {
 
   return (
     <>
-      <h1>Onboarding Details</h1>
-
-      {step === 1 ? (
-        <StepOne setStep={setStep} form={form} handleChange={handleChange} />
-      ) : step === 2 ? (
-        <StepTwo setStep={setStep} form={form} handleChange={handleChange} />
-      ) : (
-        <StepThree form={form} handleChange={handleChange} />
-      )}
+      <Todo />
     </>
+    // <>
+    //   <h1>Onboarding Details</h1>
+
+    //   {step === 1 ? (
+    //     <StepOne setStep={setStep} form={form} handleChange={handleChange} />
+    //   ) : step === 2 ? (
+    //     <StepTwo setStep={setStep} form={form} handleChange={handleChange} />
+    //   ) : (
+    //     <StepThree form={form} handleChange={handleChange} />
+    //   )}
+    // </>
   );
 }
 
---
+++ src/components/Todo.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos",
+      );
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //   useEffect(1);
+
+  useEffect(() => {
+    // fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+    //   res.json().then((data) => {
+    //     console.log(data);
+    //   }),
+    // );
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <div>
+        {todos.map((todo) => {
+          console.log(todo);
+          return (
+            <div
+              className=""
+              style={{
+                borderBottom: "solid 1px red",
+              }}
+              key={todo.id}
+            >
+              <p>ID - {todo.id}</p>
+              <p>Title - {todo.title}</p>
+              <p>Completed - {todo.completed ? "Yes" : "No"}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;

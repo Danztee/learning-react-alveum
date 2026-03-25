a81f479f75a0841b7f1dd3eb84bcb9f8935fafb9
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-03-25 14:29:43 +0100

    feat: Add click handler and display click count using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import StepOne from "./components/steps/stepOne";
 import StepThree from "./components/steps/StepThree";
 import StepTwo from "./components/steps/StepTwo";
 import Todo from "./components/Todo";
 
+// const countStep = () => {
+//   const [step, setStep] = useState(1);
+// };
+
 function App() {
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
 
-  const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    address: "",
-    city: "",
-  });
+  // const ref = useRef("1");
 
-  const handleChange = (e) => {
-    // console.log(e, "event.....");
-    // console.log(e.target.name, e.target.value);
+  // ref.current = "2";
 
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+  // // console.log(ref.current);
+
+  // const [form, setForm] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   phone: "",
+  //   address: "",
+  //   city: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   // console.log(e, "event.....");
+  //   // console.log(e.target.name, e.target.value);
+
+  //   setForm({ ...form, [e.target.name]: e.target.value });
+  // };
 
   // console.log(form, "form.....");
 
+  const ref = useRef(1);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    ref.current = ref.current + 1;
+
+    alert("you clicked me " + ref.current + " times");
+
+    console.log("hi");
+    console.log(ref.current, "ref.....");
+  };
+
   return (
     <>
-      <Todo />
+      <button onClick={handleClick}>Click me</button>
+
+      <p>number : {ref.current}</p>
+
+      {/* / <Todo /> */}
     </>
     // <>
     //   <h1>Onboarding Details</h1>

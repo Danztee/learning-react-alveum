15978a7ec1bb6e375360e63fb69fa84754314a0b
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-04-22 13:49:05 +0100

    feat: use reducer class

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+// import { useRef } from "react";
+import { useReducer } from "react";
 import "./App.css";
 import StepOne from "./components/steps/stepOne";
 import StepThree from "./components/steps/StepThree";
@@ -9,7 +10,26 @@ import Todo from "./components/Todo";
 //   const [step, setStep] = useState(1);
 // };
 
+// "ADD" and "REMOVE" and "CLEAR"
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.item];
+
+    case "REMOVE":
+      return state.filter((item, index) => index !== action.index);
+
+    case "CLEAR":
+      return [];
+
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [cart, dispatch] = useReducer(reducer, []);
+
   // const [step, setStep] = useState(1);
 
   // const ref = useRef("1");
@@ -36,36 +56,43 @@ function App() {
 
   // console.log(form, "form.....");
 
-  const ref = useRef(1);
+  // const ref = useRef(1);
 
-  const handleClick = (e) => {
-    e.preventDefault();
+  // const handleClick = (e) => {
+  //   e.preventDefault();
 
-    ref.current = ref.current + 1;
+  //   ref.current = ref.current + 1;
 
-    alert("you clicked me " + ref.current + " times");
+  //   alert("you clicked me " + ref.current + " times");
 
-    console.log("hi");
-    console.log(ref.current, "ref.....");
-  };
+  //   console.log("hi");
+  //   console.log(ref.current, "ref.....");
+  // };
 
   return (
-    <>
-      {/* <button onClick={handleClick}>Click me</button> */}
-      {/* <p>number : {ref.current}</p> */}
-      <Todo />
-    </>
-    // <>
-    //   <h1>Onboarding Details</h1>
+    <div>
+      <h2>CART ITEMS: {cart.length}</h2>
 
-    //   {step === 1 ? (
-    //     <StepOne setStep={setStep} form={form} handleChange={handleChange} />
-    //   ) : step === 2 ? (
-    //     <StepTwo setStep={setStep} form={form} handleChange={handleChange} />
-    //   ) : (
-    //     <StepThree form={form} handleChange={handleChange} />
-    //   )}
-    // </>
+      <button onClick={() => dispatch({ type: "ADD", item: "Shoe" })}>
+        Add shoe
+      </button>
+
+      <button onClick={() => dispatch({ type: "ADD", item: "Cloth" })}>
+        Add cloth
+      </button>
+
+      <button onClick={() => dispatch({ type: "REMOVE", index: 0 })}>
+        Remove first item
+      </button>
+
+      <button onClick={() => dispatch({ type: "CLEAR" })}>clear cart</button>
+
+      <ul>
+        {cart.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
